4e89556ce6f6e644afcbd235059815b5041c4739
Mohamed Tarakan <moetarakan@mohameds-air.lan>
2024-03-05 17:15:23 -0500

    Completed counter and todo list

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import Counter from './Counter'
+import Todo from './Todo'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <h2>Counter (+/-)</h2>
       <Counter />
+      <h2>To Do List</h2>
+      <Todo />
     </div>
   );
 }
